ee79db4697fdc753b108532d5cd26b24211d7672
andreacd20 <136090147+andreacd20@users.noreply.github.com>
2023-06-16 20:40:04 +0700

    Add files via upload

---
+++ password_generator.py
@@ -0,0 +1,11 @@
+import random
+
+char = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+length = int(input("Pass length:"))
+
+password = ""
+
+for i in range(length):
+    password += random.choice(char)
+
+print(password)
